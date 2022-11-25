6698470d7a87ed5aa9176e7b362b0fe13ca288ef
Halenark <rich21kong@yahoo.Co.uk>
2022-11-25 12:18:58 +0000

    Update embed-without-loot.py

--- embed-without-loot.py
+++ embed-without-loot.py
@@ -11,10 +11,10 @@ import time
 minitemprice = 50000000
 
 # Add your Corp or Alliance ID
-allianceID = 99001105
-allianceName = "Seventh Sanctum."
-corporationID = 98512964
-allalliancekillswebhookurl = "https://discord.com/api/webhooks/1040550494185209948/t6cmlErFvq95y61g-hClAFAADiZWMdMKtwIxQ7M1qaLPjqUM42vdAaAnG_9GYzoCm14v"
+allianceID = ""
+allianceName = ""
+corporationID = ""
+allalliancekillswebhookurl = ""
 
 global toggle
 toggle = False
